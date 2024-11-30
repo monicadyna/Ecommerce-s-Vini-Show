c83451cbf2ed0ea17ecc699548433d54ee9c778e
Mônica Dyna <monicadyna@gmail.com>
2024-11-30 11:41:10 -0300

    Update order_reviews.sql

--- sql_scripts/transactional_db_scripts/order_reviews.sql
+++ sql_scripts/transactional_db_scripts/order_reviews.sql
@@ -1,1 +1,12 @@
+CREATE TABLE order_reviews
+(
+    review_id VARCHAR(32) NOT NULL
+    , order_id VARCHAR(32) NOT NULL
+    , review_score INT
+    , review_comment_title VARCHAR(255)
+    , review_comment_message TEXT
+    , review_creation_date DATE
+    , review_answer_timestamp TIMESTAMP
+    , PRIMARY KEY (review_id)
+);
 
