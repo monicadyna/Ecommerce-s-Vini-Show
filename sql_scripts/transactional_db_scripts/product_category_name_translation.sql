ec30baa21d7ce642076a573de26942f0f2011873
Mônica Dyna <monicadyna@gmail.com>
2024-11-30 12:28:04 -0300

    Update product_category_name_translation.sql

--- sql_scripts/transactional_db_scripts/product_category_name_translation.sql
+++ sql_scripts/transactional_db_scripts/product_category_name_translation.sql
@@ -1,1 +1,7 @@
+CREATE TABLE product_category_name_translation
+(
+    product_category_name VARCHAR(50) NOT NULL
+    , product_category_name_english VARCHAR(50) NOT NULL
+    , PRIMARY KEY (product_category_name)
+);
 
