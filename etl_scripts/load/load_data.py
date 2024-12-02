9a59f9f47caeb3837be55cfb07cc967a596120e9
Henrique Arduini <arduinni@gmail.com>
2024-12-02 09:40:12 -0300

    Linting

--- etl_scripts/load/load_data.py
+++ etl_scripts/load/load_data.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import pyodbc
 
-#Configuração de Conexão
-server = 'localhost'                #Servidor
-database = 'VinishowEcommerce_transacional'                  #Banco de dados
-username = 'SA'                     #Usuário
-password = '123@Mudar'              #Senha
-table_name = ''                     #Tabela
+# Configuração de Conexão
+server = "localhost"  # Servidor
+database = "VinishowEcommerce_transacional"  # Banco de dados
+username = "SA"  # Usuário
+password = "123@Mudar"  # Senha
+table_name = ""  # Tabela
 
 # String de conexão
 conn_str = (
@@ -17,36 +17,36 @@ conn_str = (
     f"PWD={password}"
 )
 
-#Conexão do banco de dados
+# Conexão do banco de dados
 conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
 
 datasets = {
-    'transac_data.customers': './ingestion/olist_customers_dataset.csv',
-    'transac_data.geolocation': './ingestion/olist_geolocation_dataset.csv',
-    'transac_data.order_items': './ingestion/olist_order_items_dataset.csv',
-    'transac_data.payments': './ingestion/olist_order_payments_dataset.csv',
-    'transac_data.order_reviews': './ingestion/olist_order_reviews_dataset.csv',
-    'transac_data.orders': './ingestion/olist_orders_dataset.csv',
-    'transac_data.products': './ingestion/olist_products_dataset.csv',
-    'transac_data.sellers': './ingestion/olist_sellers_dataset.csv',
-    'transac_data.product_category_name_translation': './ingestion/product_category_name_translation.csv'
+    "transac_data.customers": "./ingestion/olist_customers_dataset.csv",
+    "transac_data.geolocation": "./ingestion/olist_geolocation_dataset.csv",
+    "transac_data.order_items": "./ingestion/olist_order_items_dataset.csv",
+    "transac_data.payments": "./ingestion/olist_order_payments_dataset.csv",
+    "transac_data.order_reviews": "./ingestion/olist_order_reviews_dataset.csv",
+    "transac_data.orders": "./ingestion/olist_orders_dataset.csv",
+    "transac_data.products": "./ingestion/olist_products_dataset.csv",
+    "transac_data.sellers": "./ingestion/olist_sellers_dataset.csv",
+    "transac_data.product_category_name_translation": "./ingestion/product_category_name_translation.csv",
 }
 
-#Insert dos datasets
+# Insert dos datasets
 for table_name, dataset in datasets.items():
     df = pd.read_csv(dataset)
     for index, row in df.iterrows():
         # Inserção
-        columns = ', '.join(row.index)
-        placeholders = ', '.join('?' for _ in row)
+        columns = ", ".join(row.index)
+        placeholders = ", ".join("?" for _ in row)
         sql = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
         values = tuple(row)
         cursor.execute(sql, values)
 
-#Commit
+# Commit
 conn.commit()
 
-#Fechar Conexão
+# Fechar Conexão
 cursor.close()
 conn.close()
